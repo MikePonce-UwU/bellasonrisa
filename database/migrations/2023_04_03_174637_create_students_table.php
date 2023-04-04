a485ddc92030e02c841638f868f7023cb4f92136
MikePonce-UwU <mvallecillo101@gmail.com>
2023-04-04 15:55:41 -0600

    añadiendo las primeras migraciones

--- database/migrations/2023_04_03_174637_create_students_table.php
+++ database/migrations/2023_04_03_174637_create_students_table.php
@@ -11,13 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('students', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('nombre_completo');
+            $table->string('fecha_nacimiento');
+            $table->string('telefono')->nullable();
+            $table->string('contacto_emergencia')->nullable();
+            $table->string('cedula')->nullable();
+            $table->string('sexo')->default('masculino');
             $table->timestamps();
             $table->softDeletes();
         });
@@ -28,6 +29,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('students');
     }
 };
